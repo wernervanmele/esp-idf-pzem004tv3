a6ddbc6296a334df6cea70780bb497aaa62c5e44
Werner Van Mele <wvanmele@gmail.com>
2023-05-29 10:41:30 +0200

    Updated example

--- examples/basic_measurements/main.c
+++ examples/basic_measurements/main.c
@@ -9,31 +9,26 @@
 /* @brief Set ESP32  Serial Configuration */
 pzemSetup_t pzConf =
 {
-    .pzem_uart   = UART_NUM_2,              /*  <== Specify the UART you want to use */
+    .pzem_uart   = UART_NUM_2,              /*  <== Specify the UART you want to use, UART_NUM_0, UART_NUM_1, UART_NUM_2 (ESP32 specific) */
     .pzem_rx_pin = GPIO_NUM_16,             /*  <== GPIO for RX */
     .pzem_tx_pin = GPIO_NUM_17,             /*  <== GPIO for TX */
-    .pzem_addr   = PZ_DEFAULT_ADDRESS,
+    .pzem_addr   = PZ_DEFAULT_ADDRESS,      /*  If your module has a different address, specify here or update the variable in pzem004tv3.h */
 };
 
-
-/* Declare Functions */
-void PMonTask( void * pz );
-
-/* Define a Log TAG */
 static const char * TAG = "APP_MAIN";
 TaskHandle_t PMonTHandle = NULL;
 _currentValues_t pzValues;            /* Measured values */
 
+void PMonTask( void * pz );
 
 void app_main()
 {
-    esp_log_level_set( "*", ESP_LOG_INFO );
-
     /* Initialize/Configure UART */
     PzemInit( &pzConf );
 
     xTaskCreate( PMonTask, "PowerMon", ( 256 * 8 ), NULL, tskIDLE_PRIORITY, &PMonTHandle );
 }
+
 
 
 void PMonTask( void * pz )
